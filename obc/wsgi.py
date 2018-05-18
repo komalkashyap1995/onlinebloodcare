40ed835417cffba54e05d77dab6f5c7d05459173
komalkashyap1995 <39401443+komalkashyap1995@users.noreply.github.com>
2018-05-18 18:26:15 +0530

    Update wsgi.py

--- obc/wsgi.py
+++ obc/wsgi.py
@@ -8,18 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
-path = '/home/komalkashyap1995/obc'
-if path not in sys.path:
-	sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'obc.settings'
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "obc.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "obc.settings")
 
-# application = get_wsgi_application()
+application = get_wsgi_application()
