c57cd4774fe851ed30b2d46b2b8bffcd63135fe3
komalkashyap1995 <39401443+komalkashyap1995@users.noreply.github.com>
2018-05-18 18:51:06 +0530

    Update sample_wsgi.py

--- sample_wsgi.py
+++ sample_wsgi.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-path = '/home/onlinebloodcare/obc'
+path = '/home/pythonanywhereusername/projectname'
 if path not in sys.path:
 	sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'obc.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'projectname.settings'
 
 from django.core.wsgi import get_wsgi_application
 from django.contrib.staticfiles.handlers import StaticFilesHandler
